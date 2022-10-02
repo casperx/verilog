fab088a222d131c0efea4d7bc9e19ca2df700ace
CasperX <casperx102@gmail.com>
2022-10-02 13:40:14 +0700

    Update sar_adc.v

--- sar_adc.v
+++ sar_adc.v
@@ -1,9 +1,7 @@
-module sar_adc #
-(
+module sar_adc #(
     // adc bits
     parameter p_bit_cnt = 8
-)
-(
+)(
     input i_clk,
     input i_reset,
 
@@ -13,16 +11,20 @@ module sar_adc #
 
     // result port
     output [p_bit_cnt - 1:0] o_res,
-    output                   o_valid,
+    output o_valid,
 
     // ports for DAC and comparator
     output [p_bit_cnt - 1:0] o_dac,
-    input                    i_cmp
+    input i_cmp
 );
     localparam p_top_bit = p_bit_cnt - 1;
 
     // states
-    localparam s_idle = 2'b0, s_samp = 2'b1, s_conv = 2'b2, s_done = 2'b3;
+    localparam
+        s_idle = 2'b0,
+        s_samp = 2'b1,
+        s_conv = 2'b2,
+        s_done = 2'b3;
 
     reg [1:0] r_state;
 
@@ -38,59 +40,31 @@ module sar_adc #
     assign o_dac = r_res | r_cur;
 
     initial
-
         r_state <= s_idle;
 
-    always @(posedge i_clk)
-
-    begin
-
+    always @(posedge i_clk) begin
         if (i_reset)
-
             r_state <= s_idle;
-
         else
-
-        begin
-
             case (r_state)
-
             s_idle, s_done:
-
                 if (i_start)
-
                     r_state <= s_samp;
 
-            s_samp:
-
-            begin
-
+            s_samp: begin
                 r_state <= s_conv;
-
                 r_cur <= 1 << p_bit_cnt;
-
                 r_res <= 0;
-
             end
 
-            s_conv:
-
-            begin
-
+            s_conv: begin
                 if (i_cmp)
-
                     r_res <= r_res | r_cur;
-
+                if (w_last)
+                    r_state <= s_done;
                 r_cur <= r_cur >> 1;
-
-                if (w_last) r_state <= s_done;
-
             end
-
             endcase
-
-        end
-
     end
 
 endmodule
