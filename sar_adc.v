c6db2f3911d97c7b605891f45b405c75475f031d
CasperX <casperx102@gmail.com>
2022-11-19 15:15:19 +0700

    Update sar_adc.v

--- sar_adc.v
+++ sar_adc.v
@@ -1,70 +1,70 @@
 module sar_adc #(
-    // adc bits
-    parameter p_bit_cnt = 8
+  // adc bits
+  parameter p_bit_cnt = 8
 )(
-    input i_clk,
-    input i_reset,
+  input i_clk,
+  input i_reset,
 
-    // control port
-    input  i_start,
-    output o_busy,
+  // control port
+  input  i_start,
+  output o_busy,
 
-    // result port
-    output [p_bit_cnt - 1:0] o_res,
-    output o_valid,
+  // result port
+  output [p_bit_cnt - 1:0] o_res,
+  output o_valid,
 
-    // ports for DAC and comparator
-    output [p_bit_cnt - 1:0] o_dac,
-    input i_cmp
+  // ports for DAC and comparator
+  output [p_bit_cnt - 1:0] o_dac,
+  input i_cmp
 );
-    localparam p_top_bit = p_bit_cnt - 1;
+  localparam p_top_bit = p_bit_cnt - 1;
 
-    // states
-    localparam
-        s_idle = 2'b0,
-        s_samp = 2'b1,
-        s_conv = 2'b2,
-        s_done = 2'b3;
+  // states
+  localparam
+    s_idle = 2'b0,
+    s_samp = 2'b1,
+    s_conv = 2'b2,
+    s_done = 2'b3;
 
-    reg [1:0] r_state;
+  reg [1:0] r_state;
 
-    reg [p_bit_cnt - 1:0] r_cur, r_res;
+  reg [p_bit_cnt - 1:0] r_cur, r_res;
 
-    wire w_last = r_res[0];
+  wire w_last = r_res[0];
 
-    assign o_busy = r_state == s_samp | r_state == s_conv;
+  assign o_busy = r_state == s_samp | r_state == s_conv;
 
-    assign o_res = r_res;
-    assign o_valid = r_state == s_done;
+  assign o_res = r_res;
+  assign o_valid = r_state == s_done;
 
-    assign o_dac = r_res | r_cur;
+  assign o_dac = r_res | r_cur;
 
-    initial
-        r_state <= s_idle;
+  initial r_state <= s_idle;
 
-    always @(posedge i_clk) begin
-        if (i_reset)
-            r_state <= s_idle;
-        else
-            case (r_state)
-            s_idle, s_done:
-                if (i_start)
-                    r_state <= s_samp;
+  always @(posedge i_clk) begin
+    if (i_reset)
+      r_state <= s_idle;
 
-            s_samp: begin
-                r_state <= s_conv;
-                r_cur <= 1 << p_bit_cnt;
-                r_res <= 0;
-            end
+    else
+      case (r_state)
+        s_idle, s_done:
+          if (i_start)
+            r_state <= s_samp;
 
-            s_conv: begin
-                if (i_cmp)
-                    r_res <= r_res | r_cur;
-                if (w_last)
-                    r_state <= s_done;
-                r_cur <= r_cur >> 1;
-            end
-            endcase
-    end
+        s_samp: begin
+          r_state <= s_conv;
+          r_cur <= 1 << p_bit_cnt;
+          r_res <= 0;
+        end
 
+        s_conv: begin
+           if (i_cmp)
+             r_res <= r_res | r_cur;
+           if (w_last)
+             r_state <= s_done;
+
+           r_cur <= r_cur >> 1;
+        end
+      endcase
+  end
 endmodule
